30893565aa663ad8273ffbcfc608c80d6dd64c1f
dsp <you@example.com>
2018-07-17 20:27:10 -0700

    add WriteToFileClass

--- app/src/main/java/com/example/mojtaba/test2/WriteToFile.java
+++ app/src/main/java/com/example/mojtaba/test2/WriteToFile.java
@@ -1,17 +1,5 @@
 package com.example.mojtaba.test2;
 
-
-import java.text.Format;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class Utility {
-    public static String getCurrentTime(String timeformat) {
-        Format formatter = new SimpleDateFormat(timeformat);
-        return formatter.format(new Date());
-    }
-}
-
 public void WriteToFile(String data) {
 		String path = "test2";
 		String name = "test.txt";
