b7efa90b067fbf94a2cfaea4f4d8f0c4bd77d2e1
unknown <mojtabamarashee@gmail.com>
2018-07-21 06:41:56 +0430

    ok; remove alarmmanger

--- app/src/main/java/com/example/mojtaba/test2/Title.java
+++ app/src/main/java/com/example/mojtaba/test2/Title.java
@@ -38,21 +38,33 @@ public class Title extends AsyncTask<Void, Void, Void> {
 
             Document document = Jsoup.connect("http://www.tgju.org/coin").timeout(50 * 1000).get();
             WriteToFile.Write("in Runnable after jsoup");
-            Elements a = document.body().select("*");
+            Elements a = document.body().select("*");            WriteToFile.Write("in Runnable after jsoup");
+            WriteToFile.Write("in Runnable after2");
 
-            title= a.select("body > main > div+ div  table> tbody > tr + tr >th").get(0).text();
-            title += ":";
+
+            //title= a.select("body > main > div+ div  table> tbody > tr + tr >th").get(0).text();
+            title += "سکه امامی:";
             title += a.select("body > main > div+ div  table> tbody > tr + tr >th + td").get(0).text();
             title += "\n";
+            WriteToFile.Write("in Runnable after3");
+
 
             Calendar c = Calendar.getInstance();
+            WriteToFile.Write("in Runnable after4");
+
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            WriteToFile.Write("in Runnable after5");
+
             String strDate = sdf.format(c.getTime());
+            WriteToFile.Write("in Runnable after6");
+
 
             title += strDate;
 
             title += "\n";
             title += Integer.toString(cntr);
+            WriteToFile.Write("in Runnable after7");
+
             cntr = cntr + 1;
             WriteToFile.Write("in Runnable at End");
 
@@ -73,7 +85,6 @@ public class Title extends AsyncTask<Void, Void, Void> {
         ComponentName thiswidget = new ComponentName(context, NewAppWidget.class);
         AppWidgetManager manager = AppWidgetManager.getInstance(context);
         manager.updateAppWidget(thiswidget, views);
-
     }
 
 
