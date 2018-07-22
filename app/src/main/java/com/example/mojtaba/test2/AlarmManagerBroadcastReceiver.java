f783c2e95fd611ba819825d1edce169a53599492
dsp <you@example.com>
2018-07-22 18:50:55 +0330

    add handlerId class

--- app/src/main/java/com/example/mojtaba/test2/AlarmManagerBroadcastReceiver.java
+++ app/src/main/java/com/example/mojtaba/test2/AlarmManagerBroadcastReceiver.java
@@ -1,5 +1,4 @@
 package com.example.mojtaba.test2;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -22,66 +21,53 @@ import java.text.SimpleDateFormat;
 
 public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
 
+	int cntr = 0;
+	@Override
+	public void onReceive(final Context context, Intent intent) {
+		PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+		PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG");
+		//Acquire the lock
+		wl.acquire();
+		Toast.makeText(context, "on recive", Toast.LENGTH_LONG).show();
 
+		//You can do the processing here update the widget/remote views.
+		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+				R.layout.new_app_widget);
+		final Context contextt = context;
+		final Handler handler = new Handler();
 
+		Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				String title = "loading";
+				try{
 
+					//Toast.makeText(contextt, "onRunnable", Toast.LENGTH_LONG).show();
+					WriteToFile.Write("onRuunable");
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+						new Title(context).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+					}
+					else {
 
+						new Title(context).execute();
 
+					}
 
+					handler.postDelayed(this, 60  * 1000);
+				}
 
-    int cntr = 0;
- @Override
- public void onReceive(final Context context, Intent intent) {
-  PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-  PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG");
-  //Acquire the lock
-  wl.acquire();
-  Toast.makeText(context, "on recive", Toast.LENGTH_LONG).show();
+				catch(Exception e)
+				{
+					//WriteToFile("run Exeption");
+					title="error : " + e.getMessage()+ "title :"  + title;
+					Toast.makeText(contextt, title, Toast.LENGTH_LONG).show();
+					WriteToFile.Write(title);
 
-  //You can do the processing here update the widget/remote views.
-  final RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-          R.layout.new_app_widget);
-     final Context contextt = context;
-     final Handler handler = new Handler();
-     Runnable r = new Runnable() {
+				}
+			}
+		};
 
-   @Override
-   public void run() {
-       String title = "loading";
-    try{
-
-        //Toast.makeText(contextt, "onRunnable", Toast.LENGTH_LONG).show();
-        WriteToFile.Write("onRuunable");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            new Title(context).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-        }
-        else {
-
-            new Title(context).execute();
-
-        }
-
-        handler.postDelayed(this, 60  * 1000);
-        //}
-    }
-    catch(Exception e)
-    {
-        //WriteToFile("run Exeption");
-        title="error : " + e.getMessage()+ "title :"  + title;
-        Toast.makeText(contextt, title, Toast.LENGTH_LONG).show();
-        WriteToFile.Write(title);
-
-    }
-       //rm.setTextViewText(R.id.appwidget_text, Utility.getCurrentTime("hh:mm:ss a"));
-
-
-
-     //  handler.postDelayed(this, 1   * 1000);
-   }
-  };
-  handler.post(r);
-
-
-  wl.release();
- }
+		handler.post(r);
+		wl.release();
+	}
 }
--- app/src/main/java/com/example/mojtaba/test2/NewAppWidget.java
+++ app/src/main/java/com/example/mojtaba/test2/NewAppWidget.java
@@ -27,52 +27,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RemoteViews;
 import android.widget.Toast;
- 
-
-
-
-
-
-
-/**
- * Implementation of App Widget functionality.
- * App Widget Configuration implemented in {@link NewAppWidgetConfigureActivity NewAppWidgetConfigureActivity}
- */
-
 
 public class NewAppWidget extends AppWidgetProvider {
 	static String title;
 	int cntr = 0;
 	public static Context context;
 
-
     static void updateAppWidget(final Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-		//RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-		//	views.setTextViewText(R.id.appwidget_text, "test2");
-		//	appWidgetManager.updateAppWidget(appWidgetId, views);
-
-
-		//AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-		//Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
-		//PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-		////After after 3 seconds
-		//am.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ 1 , pi);
 		Toast.makeText(context, "onEnable", Toast.LENGTH_LONG).show();
+		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+		final HandlerWithID handler = new Handler();
 
-
-
-		final RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-				R.layout.new_app_widget);
-		final Handler handler = new Handler();
 		Runnable r = new Runnable() {
-
 			@Override
 			public void run() {
 				String title = "loading";
 				try {
 
-					//Toast.makeText(contextt, "onRunnable", Toast.LENGTH_LONG).show();
 					WriteToFile.Write("onRuunable");
 					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 						new Title(context).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
@@ -80,22 +52,21 @@ public class NewAppWidget extends AppWidgetProvider {
 
 						new Title(context).execute();
 					}
+
 					handler.postDelayed(this, 60 * 1000);
-					//}
-				} catch (Exception e) {
-					//WriteToFile("run Exeption");
+				}catch (Exception e) {
+					//WriteToFile.Write("run Exeption");
 					title = "error : " + e.getMessage() + "title :" + title;
 					Toast.makeText(context, title, Toast.LENGTH_LONG).show();
 					WriteToFile.Write(title);
 
 				}
-				//rm.setTextViewText(R.id.appwidget_text, Utility.getCurrentTime("hh:mm:ss a"));
 			}
 		};
 
 		handler.post(r);
-
 	}
+
 	@Override
 	public void onEnabled(Context context) {
 		super.onEnabled(context);
@@ -110,25 +81,11 @@ public class NewAppWidget extends AppWidgetProvider {
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
-		//ComponentName thisWidget = new ComponentName(context,
-		//		TimeWidgetProvider.class);
-
-		//for (int widgetId : appWidgetManager.getAppWidgetIds(thisWidget)) {
 			for (int appWidgetId : appWidgetIds) {
-
-				//Get the remote views
-				//RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-				//R.layout.time_widget_layout);
-				// Set the text with the current time.
-				//remoteViews.setTextViewText(R.id.tvTime, Utility.getCurrentTime("hh:mm:ss a"));
-				//appWidgetManager.updateAppWidget(widgetId, remoteViews);
-
 				RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
 				views.setTextViewText(R.id.appwidget_text, "test");
 				appWidgetManager.updateAppWidget(appWidgetId, views);
 				Toast.makeText(context, "onUpdate", Toast.LENGTH_LONG).show();
-
-
 		}
 	}
  
